1840853a852d320eb08a3e1a5057a258fbc956f8
mikas <mikasisto2@gmail.com>
2018-01-16 11:17:59 +0000

    pagina de registar feita - consertar bugs.

--- app/controllers/conta_controller.rb
+++ app/controllers/conta_controller.rb
@@ -1,4 +1,31 @@
 class ContaController < ApplicationController
+
+  def show
+    @conta = Conta.find(params[:id])
+  end
+
+  def new
+    @contanova = Conta.new
+  end
+
+  def index
+    @contas = Conta.all
+  end
+
   def view
+  end
+
+  def create
+  @conta = Conta.new(params[:conta])    # Not the final implementation!
+    if @conta.save
+      # Handle a successful save.
+    else
+      render 'new'
+    end
+  end
+
+  def conta_params
+  params.require(:conta).permit(:name, :email, :password,
+                               :password_confirmation)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,17 @@
 Rails.application.routes.draw do
+
+
+
   get 'review/view'
 
   get 'jogo/view'
 
   get 'conta/view'
-
+  get 'conta/index'
   get 'noticias/view'
 
   root 'frontoffice#index'
+  get  '/signup',  to: 'conta#new'
+  resources :conta
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
